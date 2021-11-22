c137246dd0482d1d57b688e55f961909972c1d6d
Mikaio Faria <mfaria@gobetterfly.com>
2021-11-22 12:18:51 -0300

    feat:flow_ux_for_seller

--- core/urls.py
+++ core/urls.py
@@ -7,9 +7,13 @@ from django.conf.urls.static import static
 app_name = 'ingresschannel'
 urlpatterns = [
     path('', core_views.home, name='home'),
+    path('policies', core_views.policies, name='policies'),
+    # Buyer URLs
     path('event', core_views.event, name='event'),
-    path('dashboard', core_views.dashboard, name='dashboard')
-    # path('buy/ticket')
-    # path('advetiser')
+    # Seller URLs
+    path('dashboard', core_views.dashboard, name='dashboard'),
+    path('create_event', core_views.create_event, name='create_event'),
+    path('withdraw_historic', core_views.withdraw_historic, name='withdraw_historic'),
+    path('report', core_views.report, name='report'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
+def policies(request):
+    return render(request, 'policies.html', {})
+
 def event(request):
     return render(request, 'event.html', {})
 
 def dashboard(request):
     return render(request, 'dashboard.html', {})
+
+def create_event(request):
+    return render(request, 'create_event.html', {})
+
+def withdraw_historic(request):
+    return render(request, 'withdraw_historic.html', {})
+
+def report(request):
+    return render(request, 'report.html', {})
